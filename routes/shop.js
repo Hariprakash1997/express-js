f11ece8d5147692d7fab7c439c277a11432bea86
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-27 13:35:05 +0530

    adding template engine handlebarjs

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,13 @@ router.get('/', (req, res, next) => {
     // res.sendFile(path.join(rootDir ,'views', 'shop.html'));
     // console.log(adminData.products);
     const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop', 
+        path: '/', hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
