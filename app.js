fd0d23dec358cdc0fde788c5c4febf7c9646485c
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-27 14:12:04 +0530

    adding template engine ejs

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');  
-const expressHbs = require('express-handlebars');
+
+//------for hbs-------
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
+//------for pug------- 
+
 // app.set('view engine', 'pug');
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts', 
-    defaultLayout: 'main-layout', 
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+
+//------for hbs-----
+
+// app.engine('hbs', expressHbs({
+//     layoutsDir: 'views/layouts', 
+//     defaultLayout: 'main-layout', 
+//     extname: 'hbs'
+// }));
+// app.set('view engine', 'hbs');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
