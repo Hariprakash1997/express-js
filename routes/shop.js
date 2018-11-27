e04385bacdbb8fb8dfab8ce30b07706d8416afc1
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-27 12:32:24 +0530

    adding templating engine pug

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir ,'views', 'shop.html'));
+    // res.sendFile(path.join(rootDir ,'views', 'shop.html'));
+    // console.log(adminData.products);
+    const products = adminData.products;
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
